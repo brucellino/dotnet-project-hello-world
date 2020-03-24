f67133e783053495047aa6644d551b66f2ef877f
Bruce Becker <bruce.becker@uefa.ch>
2020-03-24 07:15:43 +0100

    Add new function, README
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

--- Program.cs
+++ Program.cs
@@ -2,16 +2,50 @@
 
 namespace hello_world {
   class Program {
-    static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
-      Console.WriteLine("The time is " + DateTime.Now);
-      Numbers();
-    }
+
     static void Numbers() {
       int a = 18;
       int b = 6;
       int c = a + b;
       Console.WriteLine(c);
+    }
+
+    static void OrderOfPrecedence() {
+      int a = 5;
+      int b = 4;
+      int c = 2;
+      int d = a + b + c;
+
+      Console.WriteLine(d);
+
+      d = (a+b)*c;
+      Console.WriteLine(d);
+
+      int e = 7;
+      int f = 4;
+      int g = 3;
+      int h = (e + f)/g;
+      Console.WriteLine(h);
+
+    }
+
+    static void QuotientRemandiner() {
+
+        int a = 7;
+        int b = 4;
+        int c = 3;
+        int d = (a + b) / c;
+        int e = (a + b) % c;
+
+        Console.WriteLine($"quotient {d}");
+        Console.WriteLine($"remainder {e}");
+    }
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("The time is " + DateTime.Now);
+      Numbers();
+      OrderOfPrecedence();
+      QuotientRemandiner();
     }
   }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# C# practice
+
+This is a litte repo to help me go through the C# tutorials.
+
+## Contributing
+
+Get your own little repo to help you go through the C# tutorials.
