3c28d37f73da95e904f3682296570c6ebcebe366
Bruce Becker <bruce.becker@uefa.ch>
2020-03-24 16:45:20 +0100

    Ignore obj dirs in any subdirectory
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

---
+++ console/branches/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace branches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
