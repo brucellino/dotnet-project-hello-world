029b56e8467f453d9518b43bfd04b1e4d21139c4
Bruce Becker <bruce.becker@uefa.ch>
2020-03-24 07:03:25 +0100

    Initial commit
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hello_world {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("The time is " + DateTime.Now);
+      Numbers();
+    }
+    static void Numbers() {
+      int a = 18;
+      int b = 6;
+      int c = a + b;
+      Console.WriteLine(c);
+    }
+  }
+}
