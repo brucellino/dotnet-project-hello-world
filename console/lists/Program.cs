e639a5a1be3e54b6545a6d95d127448a04a2dd3c
Bruce Becker <bruce.becker@uefa.ch>
2020-03-25 08:47:17 +0100

    Check how indices work
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

--- console/lists/Program.cs
+++ console/lists/Program.cs
@@ -12,6 +12,24 @@ namespace lists
       foreach (var name in names) {
         Console.WriteLine($"Hello {name.ToUpper()}!");
       }
+      var index = names.IndexOf("Felipe");
+      if (index == -1) {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+      } else {
+        Console.WriteLine($"Then name {names[index]} is at index {index}");
+      }
+
+      index = names.IndexOf("Not Found");
+      if (index == -1) {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+      } else {
+        Console.WriteLine($"Then name {names[index]} is at index {index}");
+      }
+
+      names.Sort();
+      foreach (var name in names) {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
     }
   }
 }
