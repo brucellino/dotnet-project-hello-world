83df5ee3907e1fe606d260436da4f2651f7d972c
Bruce Becker <brucellino@gmail.com>
2020-03-25 10:43:19 +0100

    Implement bank account
    Signed-off-by: Bruce Becker <brucellino@gmail.com>

--- console/classes/Program.cs
+++ console/classes/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace classes
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+  public class BankAccount {
+    /*
+    <summary>
+    <c>MakeDeposit</c> makes a deposit
+
+    </summary>
+    */
+    private static int accountNumberSeed = 1234567890;
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance {
+      get {
+        decimal balance = 0;
+        foreach (var item in allTransactions) {
+          balance += item.Amount;
         }
+        return balance;
+      }
+
     }
+
+    private List<Transaction> allTransactions = new List<Transaction>();
+
+    public BankAccount(string name, decimal initialBalance) {
+      // Initialisation method
+      this.Owner = name;
+      this.Number = accountNumberSeed.ToString();
+      accountNumberSeed++;
+      MakeDeposit(initialBalance, DateTime.Now, "initial balance");
+
+    }
+    public void MakeDeposit(
+      decimal amount,
+      DateTime date,
+      string note
+    ) {
+      if(amount <= 0) {
+        throw new ArgumentOutOfRangeException(nameof(amount),
+        "Amount of deposit must be positive");
+      }
+      var deposit = new Transaction(amount, date, note);
+      allTransactions.Add(deposit);
+    }
+
+    public void MakeWithdrawal(
+      decimal amount,
+      DateTime date,
+      string note
+    ) {
+      if(amount <= 0) {
+        throw new ArgumentOutOfRangeException(nameof(amount),
+        "Amount of withdrawal must be positive");
+      }
+      if(Balance - amount < 0) {
+        throw new InvalidOperationException("Insufficient funds.");
+      }
+      var withdrawal = new Transaction(-amount,date,note);
+
+      allTransactions.Add(withdrawal);
+    }
+  }
+  class Program {
+    static void Main(string[] args) {
+      var account = new BankAccount("Bruce", 1000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial funds.");
+
+      account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+      Console.WriteLine(account.Balance);
+      account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+      Console.WriteLine(account.Balance);
+
+    }
+  }
 }
