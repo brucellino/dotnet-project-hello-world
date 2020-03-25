a8bfc853dfda89cad1bfa0aa6dc303d181c32da6
Bruce Becker <bruce.becker@uefa.ch>
2020-03-25 07:48:00 +0100

    Initial commit of lists tutorial
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

---
+++ console/lists/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace lists
+{
+  class Program {
+    static void Main(string[] args) {
+      var names = new List<string> { "<name>", "Ana", "Felipe"};
+      foreach (var name in names) {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
+    }
+  }
+}
